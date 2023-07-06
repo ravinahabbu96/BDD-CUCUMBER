6cc32cbb27a669f0baa10bb76de68ff405f367d8
ravinahabbu96 <ravina.habbu@gf.genero.jp>
2023-07-06 19:48:43 +0530

    first commit

--- cypress/integration/Amazon-Test/Amazon.feature
+++ cypress/integration/Amazon-Test/Amazon.feature
@@ -1,25 +1,26 @@
 Feature: Amazon Product Search
 
-    Scenario: Validate the product searched for shoes display the correct result
+    Scenario: TC_001_Product search functionality
         Given Access the Amazon top page url
-        When Enter Shoes keyword into searchox
-        And Hit the search icon
-        Then verify that the values on the search page are actually searched for shoes
+        When  Enter Shoes keyword into the searchox
+        And   Hit the search icon
+        Then  keyword search should display correct result
 
 
-    Scenario: Validate the two filters are applied and the results are being filtered logically.
-        Given Access the Amazon top page url
-        When Enter Shoes keyword into searchox
-        And Hit the search icon
-        And Validate the two filters applied
-        Then Results filtered logically
+    Scenario: TC_002_Apply filters and check result
+        Given  Access the Amazon top page url
+        When   Enter Shoes keyword into the searchox
+        And    Hit the search icon
+        When   Apply two filters on the same page
+        Then   Filtered results should display correct result
 
 
-    Scenario: Product Details page
-        Given Access the Amazon top page url
-        When Enter Shoes keyword into searchox
-        And Hit the search icon
-        Then Validate the product details
+    Scenario: TC_003_Add product to the cart
+        Given  Access the Amazon top page url
+        When   Enter Shoes keyword into the searchox
+        And    Hit the search icon
+        When   Apply two filters on the same page
+        Then   Product should get added to the cart successfully
 
 
 
--- cypress/integration/Amazon-Test/Amazon/Amazon.js
+++ cypress/integration/Amazon-Test/Amazon/Amazon.js
@@ -1,11 +1,11 @@
 import { Given, And, Then, When } from "cypress-cucumber-preprocessor/steps";
-let brand
-let features
+
+// First Scenario
 Given('Access the Amazon top page url', () => {
     cy.visit("https://www.amazon.in")
 })
 
-When('Enter Shoes keyword into searchox', () => {
+When('Enter Shoes keyword into the searchox', () => {
     cy.get('input#twotabsearchtextbox').type('shoes')
 })
 
@@ -13,73 +13,52 @@ And('Hit the search icon', () => {
     cy.get('input[type=submit]').click()
 })
 
-Then('verify that the values on the search page are actually searched for shoes', () => {
+Then('keyword search should display correct result', () => {
     cy.url().should('include', 'shoes')
+    cy.wait(2000)
+
 })
 
-// When('Apply filters on the same page', () => {
-//     cy.get('div[id="brandsRefinements"]>ul>span>li>span>a>div').eq(0).click({ force: true })
+// **********************************************************************************************
+
+//Second Scenario//
+When('Apply two filters on the same page', () => {
+    cy.get('ul[aria-labelledby="p_89-title"]>span>li>span>a>div').eq(0).click({ force: true })
+    cy.get('ul[aria-labelledby="p_n_feature_fourteen_browse-bin-title"]>span>li>span>a>span').eq(2).click({ force: true })
+    cy.wait(4000)
+})
+
+// And('Validate the two filters applied', () => {
+//     // validated filter texts
 //     cy.wait(4000)
-//     cy.get('ul[aria-labelledby="p_n_feature_fourteen_browse-bin-title"]>span>li>span>a>span').eq(2).click({ force: true })
-
-
-
-//     cy.get('div[id="brandsRefinements"]>ul>span>li>span>a>span').eq(0).each(($el) => {
-//         brand = cy.log($el.text())
-//     })
-//     cy.wait(4000)
-//     cy.get('ul[aria-labelledby="-title"]>span>li>span>a>span').eq(0).invoke('show')
-//         .should('have.text', brand)
-
-
-//     cy.wait(4000)
-//     cy.get('ul[aria-labelledby="p_n_feature_fourteen_browse-bin-title"]>span>li>span>a>span').eq(2).each(($el) => {
-//         features = cy.log($el.text())
-//     })
-//     cy.wait(4000)
-//     cy.get('ul[aria-labelledby="-title"]>span>li>span>a>span').eq(1).invoke('show')
-//         .should('have.text', 'features')
+//     cy.get('ul.sf-selected-filters-list>span>li>span>a>span',).eq(0).should('have.text', 'Red Tape')
+//     cy.get('ul.sf-selected-filters-list>span>li>span>a>span').eq(1).should('have.text', 'Lightweight')
 // })
 
-And('Validate the two filters applied', () => {
-
-    //Two filters applied
-    cy.get('ul[aria-labelledby="p_89-title"]>span>li>span>a>div').eq(0).click({ force: true })
-    cy.wait(6000)
-    cy.get('ul[aria-labelledby="p_n_feature_fourteen_browse-bin-title"]>span>li>span>a>span').eq(2).click({ force: true })
-    cy.wait(7000)
-    // validated filter texts
-    cy.get('ul.sf-selected-filters-list>span>li>span>a>span',).eq(0).should('have.text', 'Red Tape')
-    cy.wait(8000)
-    cy.get('ul.sf-selected-filters-list>span>li>span>a>span').eq(1).should('have.text', 'Lightweight')
-
-
-})
-
-Then('Results filtered logically', () => {
+Then('Filtered results should display correct result', () => {
+    //Validate 3 results with applied filter .i.e Red Tape
+    let n = 0
     cy.get('div[class="a-section a-spacing-none a-spacing-top-small s-title-instructions-style"]').each(($el, ind) => {
-        cy.wait(5000)
-        cy.wrap($el.eq(0)).should('contain', 'Red Tape')
-        cy.wait(5000)
-        cy.wrap($el.eq(1)).should('contain', 'Red Tape')
-        cy.wait(5000)
-        cy.wrap($el.eq(3)).should('contain', 'Red Tape')
-
-
+        if (n < 4) {
+            cy.wait(2000)
+            cy.wrap($el).should('contain', 'Red Tape')
+            n++
+        }
     })
 })
 
 
-Then('Validate the product details', () => {
-    cy.get('ul[aria-labelledby="p_89-title"]>span>li>span>a>div').eq(0).click({ force: true })
-    cy.wait(6000)
-    cy.get('ul[aria-labelledby="p_n_feature_fourteen_browse-bin-title"]>span>li>span>a>span').eq(2).click({ force: true })
-    cy.wait(7000)
-    cy.get('div[class="a-section a-spacing-base a-text-center"]>div>div>span>a').eq(0).invoke('removeAttr', 'target').click()
-    cy.get('#add-to-cart-button').click()
-    cy.wait(8000)
-    cy.get('div[id="nav-cart-count-container"]>span.nav-cart-count').should('have.text', "1")
+// **********************************************************************************************
 
+
+// Third Scenario
+Then('Product should get added to the cart successfully', () => {
+    //Product is sucessfully added to the cart
+    cy.get('div[class="a-section a-spacing-base a-text-center"]>div>div>span>a').eq(0).invoke('removeAttr', 'target').click()
+    cy.get('select[name="quantity"]').select('2')
+    cy.get('#add-to-cart-button').click()
+    cy.get('.a-size-medium-plus').should('contain', 'Added to Cart')
+    cy.get('div[id="nav-cart-count-container"]>span.nav-cart-count').should('have.text', "2")
 })
 
 
